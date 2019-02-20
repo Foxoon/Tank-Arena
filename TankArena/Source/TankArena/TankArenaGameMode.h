0a9992e8a3f6d29a39f46c087b0f89682d198b0d
coret <fabien.co93@gmail.com>
2019-02-20 21:37:01 +0100

    Set up

---
+++ TankArena/Source/TankArena/TankArenaGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankArenaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKARENA_API ATankArenaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
